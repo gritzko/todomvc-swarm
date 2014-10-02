2a985de35de31acf56ae9e3dddd815538b42ad47
Victor Grishchenko <victor.grishchenko@gmail.com>
2014-10-02 17:43:53 +0400

    oops

---
+++ fake_auth_init.js
@@ -0,0 +1,6 @@
+(function() {
+    var sessionId = window.localStorage.getItem('.localuser') || 
+        'A~' + Swarm.Spec.int2base((Math.random()*10000)|0);
+    window.localStorage.setItem('.localuser',sessionId);
+    window.app = new window.TodoApp(sessionId);
+}());
