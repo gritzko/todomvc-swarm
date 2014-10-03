ea8a070fcf918135192e0b25b70ba0dadfda9f15
olebedev <oolebedev@gmail.com>
2014-10-03 19:33:49 +0600

    cli improvement

--- TodoAppServer.js
+++ TodoAppServer.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 "use strict";
 
 // node libs
@@ -25,129 +26,151 @@ var EinarosWSStream = Swarm.EinarosWSStream;
 var TodoList = require('./model/TodoList');
 var TodoItem = require('./model/TodoItem');
 
-Swarm.env.debug = true;
+function startApp(argv){
 
-var port = 8000;
+    var port = argv.param('p','port') || 8000;
+    var debug = argv.mode('debug') !== null ? argv.mode('debug') : false;
+    var dir = argv.param('d', 'dir') || '.swarm';
+    
+    Swarm.env.debug = debug;
 
-var app = express();
-app.use(compression());
-app.use(express.static('.'));
-app.use('/js/bundle.js', browserify({
-    entry: './TodoApp.js',
-    debug: false
-}));
+    var app = express();
+    app.use(compression());
+    app.use(express.static('.'));
+    app.use('/js/bundle.js', browserify({
+        entry: './TodoApp.js',
+        debug: false
+    }));
 
-// configure view rendering engine
-var jsx_options = {
-    jsx: {
-        harmony: false
-    }
-};
-app.engine('jsx', jsx_views.createEngine(jsx_options));
-app.set('view engine', 'jsx');
-app.set('views', process.cwd() + '/view');
-
-app.get(/[/+A-Za-z0-9_~]*/, function (req, res) {
-    Spec.reQTokExt.lastIndex = 0;
-    var path = req.path;
-    var rootListId = null;
-    var itemIds = [];
-    var m;
-    Spec.reQTokExt.lastIndex = 0;
-    while (m = Spec.reQTokExt.exec(path)) {
-        var id = m && m[2];
-        if (!rootListId) {
-            rootListId = id;
-        } else {
-            itemIds.push(id);
+    // configure view rendering engine
+    var jsx_options = {
+        jsx: {
+            harmony: false
         }
-    }
+    };
 
-    if (!rootListId) {
-        res.render('index', {
-            app: {path: []}
-        });
-        return;
-    }
+    app.engine('jsx', jsx_views.createEngine(jsx_options));
+    app.set('view engine', 'jsx');
+    app.set('views', process.cwd() + '/view');
 
-    function loadPath(path, listId, itemIds) {
-        console.log('loadPath(%s, %j)', listId, itemIds);
-        var list = app.swarmHost.get('/TodoList#' + listId);
-
-        list.onObjectStateReady(function(){
-            if (!list.length()) {
-                list.addObject(new TodoItem({text:'just do it'}));
-            }
-            if (!itemIds) {
-                itemIds = [fwdList.objectAt(0)._id];
-            } else if ('string' === typeof itemIds) {
-                itemIds = [itemIds];
-            }
-
-            var itemId = itemIds.shift();
-            path.push({
-                listId: listId,
-                itemId: itemId
-            });
-            var item = list.getObject(itemId);
-            if (!itemIds.length || !item.childList) {
-                res.header('Location', '/' + rootListId + '/' + path.map(function (el) {return el.itemId;}).join('/'));
-                res.render('index', {
-                    app: {path: path}
-                });
+    app.get(/[/+A-Za-z0-9_~]*/, function (req, res) {
+        Spec.reQTokExt.lastIndex = 0;
+        var path = req.path;
+        var rootListId = null;
+        var itemIds = [];
+        var m;
+        Spec.reQTokExt.lastIndex = 0;
+        while (m = Spec.reQTokExt.exec(path)) {
+            var id = m && m[2];
+            if (!rootListId) {
+                rootListId = id;
             } else {
-                loadPath(path, item.childList, itemIds);
+                itemIds.push(id);
             }
-            // TODO max delay
-        });
+        }
 
-    }
+        if (!rootListId) {
+            res.render('index', {
+                app: {path: []}
+            });
+            return;
+        }
 
-    loadPath([], rootListId, itemIds);
-});
+        function loadPath(path, listId, itemIds) {
+            console.log('loadPath(%s, %j)', listId, itemIds);
+            var list = app.swarmHost.get('/TodoList#' + listId);
 
+            list.onObjectStateReady(function(){
+                if (!list.length()) {
+                    list.addObject(new TodoItem({text:'just do it'}));
+                }
+                if (!itemIds) {
+                    itemIds = [fwdList.objectAt(0)._id];
+                } else if ('string' === typeof itemIds) {
+                    itemIds = [itemIds];
+                }
 
-// use file storage
-var fileStorage = new Swarm.FileStorage('.swarm');
+                var itemId = itemIds.shift();
+                path.push({
+                    listId: listId,
+                    itemId: itemId
+                });
+                var item = list.getObject(itemId);
+                if (!itemIds.length || !item.childList) {
+                    res.header('Location', '/' + rootListId + '/' + path.map(function (el) {return el.itemId;}).join('/'));
+                    res.render('index', {
+                        app: {path: path}
+                    });
+                } else {
+                    loadPath(path, item.childList, itemIds);
+                }
+                // TODO max delay
+            });
 
-// create Swarm Host
-app.swarmHost = new Swarm.Host('swarm~nodejs', 0, fileStorage);
-Swarm.env.localhost = app.swarmHost;
+        }
 
-// start the HTTP server
-var httpServer = http.createServer(app);
-
-httpServer.listen(port, function (err) {
-    if (err) {
-        console.warn('Can\'t start server. Error: ', err, err.stack);
-        return;
-    }
-    console.log('Swarm server started port', port);
-});
-
-// start WebSocket server
-var wsServer = new ws_lib.Server({
-    server: httpServer
-});
-
-// accept pipes on connection
-wsServer.on('connection', function (ws) {
-    var params = url.parse(ws.upgradeReq.url, true);
-    console.log('incomingWS %s', params.path);
-    app.swarmHost.accept(new EinarosWSStream(ws), { delay: 50 });
-});
-
-process.on('SIGTERM', onExit);
-process.on('SIGINT', onExit);
-process.on('SIGQUIT', onExit);
-
-function onExit(exitCode) {
-    console.log('shutting down http-server...');
-    httpServer.close();
-
-    console.log('closing swarm host...');
-    app.swarmHost.close(function () {
-        console.log('swarm host closed');
-        process.exit(exitCode);
+        loadPath([], rootListId, itemIds);
     });
+
+
+    // use file storage
+    var fileStorage = new Swarm.FileStorage(dir);
+
+    // create Swarm Host
+    app.swarmHost = new Swarm.Host('swarm~nodejs', 0, fileStorage);
+    Swarm.env.localhost = app.swarmHost;
+
+    // start the HTTP server
+    var httpServer = http.createServer(app);
+
+    httpServer.listen(port, function (err) {
+        if (err) {
+            console.warn('Can\'t start server. Error: ', err, err.stack);
+            return;
+        }
+        console.log('Swarm server started port', port);
+    });
+
+    // start WebSocket server
+    var wsServer = new ws_lib.Server({
+        server: httpServer
+    });
+
+    // accept pipes on connection
+    wsServer.on('connection', function (ws) {
+        var params = url.parse(ws.upgradeReq.url, true);
+        console.log('incomingWS %s', params.path);
+        app.swarmHost.accept(new EinarosWSStream(ws), { delay: 50 });
+    });
+
+    process.on('SIGTERM', onExit);
+    process.on('SIGINT', onExit);
+    process.on('SIGQUIT', onExit);
+
+    function onExit(exitCode) {
+        console.log('shutting down http-server...');
+        httpServer.close();
+
+        console.log('closing swarm host...');
+        app.swarmHost.close(function () {
+            console.log('swarm host closed');
+            process.exit(exitCode);
+        });
+    }
 }
+
+// cli app
+var pkg     = require('./package.json');
+var program = require('electron')('./TodoAppServer.js');
+
+program
+    .name('Server runner for ' + pkg.name)
+    .version(pkg.version)
+    .command('default')
+    .option('-p, --port [8000]', 'port for listening')
+    .option('-d, --dir [.swarm]', 'FileStorage dir')
+    .option('--debug', 'mode')
+    .action(startApp)
+
+program.parse();
+
