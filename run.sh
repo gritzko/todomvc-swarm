3b716f0b6eb94909e202cbd54635ff998167c492
Victor Grishchenko (linode) <victor.grishchenko@gmail.com>
2014-10-07 15:10:16 +0600

    pas on the args

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ npm install
 
 while [ /bin/true ]; do 
     LOGDATE=`date +%Y.%m.%d_%H:%M:%S`
-    node TodoAppServer.js > $LOGDATE.out 2> $LOGDATE.err
+    node TodoAppServer.js $@ > $LOGDATE.out 2> $LOGDATE.err
     gzip $LOGDATE.out &
     gzip $LOGDATE.err &
     sleep 1; 
